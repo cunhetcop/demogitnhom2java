b0b1c0f113ef3d5ca5d225843a0b1eba204dacde
nguyenchitinh <dophinmydinh2021@gmai.com>
2022-12-07 13:59:34 +0700

    tinh update 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         String tinh = "tinh nguyen";
+        String tinh2 ="tinhnguyen2"
         String halinh ="hello world";
         System.out.println(halinh);
         System.out.println(tinh);
+        System.out.println(tinh2);
     }
 }

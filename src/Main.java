9c7bd433efa2a4f6b1d6aa132d06e23a58c86a88
cunhetcop <nguyenhalinh.1709@gmail.com>
2022-12-07 14:10:44 +0700

    halinh update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         String halinh ="hello world";
+        String halinh2 = "ha linh 2";
         System.out.println(halinh);
+        System.out.println(halinh2);
     }
 }

a404527ddafb94ebe30bbc51523871886bbcf2d7
nguyenhaivanz <haivan090824@gmail.com>
2022-12-07 11:44:44 +0700

    sdf

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String halinh ="hello world";
-        System.out.println(halinh);
+        String haivan ="hello world";
+        System.out.println(haivan);
     }
 }
